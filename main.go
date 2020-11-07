cb821b620ca0c92e7f2eda9c76ba84323b97dd52
Vinícius Vedovotto <vinicius.vedovotto@unesp.br>
2020-11-07 14:06:54 -0300

    feat: load file

--- main.go
+++ main.go
@@ -5,7 +5,14 @@ import (
 	"net/http"
 	"log"
 	"github.com/gorilla/mux"
+	"os"
+	"encoding/csv"
+	"encoding/json"
+	model "github.com/viniciusveu/data-integration-challenge/models"
 )
+
+var companies []model.Company
+var company model.Company
 
 func getAll(w http.ResponseWriter, r *http.Request) {
 
@@ -21,6 +28,46 @@ func create(w http.ResponseWriter, r *http.Request) {
 
 func delete(w http.ResponseWriter, r *http.Request) {
 
+}
+
+func readCSV(csvName string) error  {
+	csvFile, err := os.Open(csvName)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer csvFile.Close()
+
+	csvReader := csv.NewReader(csvFile)
+	csvReader.Comma = ';'
+	csvReader.FieldsPerRecord = -1
+
+	csvLines, err := csvReader.ReadAll()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	//fmt.Println(csvLines)
+	for index, line := range csvLines {
+		if index == 0 {
+			continue
+		}
+		// fmt.Println(len(line))
+		company.Name = line[0]
+		company.Zip = line[1]
+		company.Site = ""
+
+		companies = append(companies, company)
+	}
+
+	jsonData, _ := json.Marshal(companies)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Println(string(jsonData))
+
+	return err
 }
 
 func handleRequests() {
@@ -39,6 +86,13 @@ func handleRequests() {
 
 func main() {
 	fmt.Println("Rest API v1.0 - Mux Routers")
+
+	err := readCSV("./assets/q1_catalog.csv")
+	if err != nil {
+		fmt.Println(err)
+		fmt.Println("Error on load file CSV")
+		os.Exit(1)
+	}
 
 	handleRequests()
 }
---
+++ models/Company.go
@@ -0,0 +1,7 @@
+package models
+
+type Company struct {
+	Name string `json:Name`
+	Zip string `json:Zip`
+	Site string `json:Site`
+}
