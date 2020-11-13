e841311878883dd38885da225bba62cfc9ce0cf3
Vinícius Vedovotto <vinicius.vedovotto@unesp.br>
2020-11-13 11:02:34 -0300

    fix: Docker composer error resolved

--- models/setup.go
+++ models/setup.go
@@ -2,22 +2,23 @@ package models
 
 import (
 	"fmt"
-	"gorm.io/gorm"
+
 	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
-var DB  *gorm.DB
+var DB *gorm.DB
 
 // Conecta ao banco de dados api_challenge e configura o gorm
 func ConnectDatabase() {
 
-	dsn := "root:root@tcp(172.17.0.2:3306)/api_challenge?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "root:root@tcp(mysql:3306)/api_challenge?charset=utf8mb4&parseTime=True&loc=Local"
 
 	database, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		fmt.Println(err)
 		panic("Failed to connect to database!")
-	} 
+	}
 
 	database.AutoMigrate(&Company{})
 
